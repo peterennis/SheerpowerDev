e99489e4964d0bc4c4284450012db5328728c491
peterennis <peterennis@yahoo.com>
2019-11-24 16:09:34 -0800

    chore(): update order of commands

--- AZ_Examples/PS_AzurePowerShellCommands.ps1
+++ AZ_Examples/PS_AzurePowerShellCommands.ps1
@@ -2,6 +2,18 @@
 # Ref: http://tritoneco.com/2014/02/21/fix-for-powershell-script-not-digitally-signed/
 # Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
+# Show PowerShell version information
+#$PSVersionTable
+
+# Show PowerShell version detail
+#$PSVersionTable.PSVersion
+
+# To install Azure PowerShell, run the following command in an elevated session ("Run as Administrator" on Windows)
+# Ref: https://docs.microsoft.com/en-us/powershell/azure/install-az-ps?view=azps-1.5.0
+#Install-Module -Name Az -AllowClobber
+
+# Show installed Azure PowerShell
+#Get-InstalledModule -Name Az -AllVersions | Select-Object Name,Version
 
 # Enter Azure credentials and then connect to your Azure subscription
 # NOTE: Login form is in background (bug) Ref: https://github.com/Azure/azure-powershell/issues/8476
@@ -15,52 +27,29 @@
 # Get a list of all Azure Locations and filter the JSON result for a paged list of alphabetically sorted location names 
 #(az account list-locations | ConvertFrom-Json) | Select-Object name | Sort-Object -Property name | more
 
-
-# Show PowerShell version detail
-#$PSVersionTable.PSVersion
-
-
-# To install Azure PowerShell, run the following command in an elevated session ("Run as Administrator" on Windows)
-# Ref: https://docs.microsoft.com/en-us/powershell/azure/install-az-ps?view=azps-1.5.0
-#Install-Module -Name Az -AllowClobber
-
-
-# Show installed Azure PowerShell
-#Get-InstalledModule -Name Az -AllVersions | Select-Object Name,Version
-
-
 # List available modules
 #Get-Module -ListAvailable
-
 
 # Get a list of all Resource Groups as a table
 #Get-AzResourceGroup | Format-Table
 
-
 ########## Popular PowerShell Commands ##########
-
 
 # Get build information for Windows
 #$(gin).WindowsBuildLabEx
 
-
 # Set preview for PowerShell files
 #Set-ItemProperty Registry::HKEY_CLASSES_ROOT\.ps1 -Name PerceivedType -Value text
-
 
 # Set preview for Sheerpower source files
 #Set-ItemProperty Registry::HKEY_CLASSES_ROOT\.spsrc -Name PerceivedType -Value text
 
-
 # Get-PSDrive cmdlet gets the drives in the current session
 #Get-PSDrive
 
-
 ########## Popular Docker Commands ##########
 
-
 # Ref: https://linuxize.com/post/how-to-remove-docker-images-containers-volumes-and-networks/
-
 
 # The docker system prune command will remove all stopped containers, all dangling images, and all unused networks
 #docker system prune
