00795351dd591daf423c5de281aca14ba7b12971
peterennis <peterennis@yahoo.com>
2019-11-25 16:29:33 -0800

    wip(): Create DC1

--- AZ_Examples/PS_AzureDevAD.ps1
+++ AZ_Examples/PS_AzureDevAD.ps1
@@ -23,6 +23,7 @@
 #New-AzResourceGroup -Name $rgName -Location $locName
 
 # Create a virtual network to host a subnet of the lab enterprise environment and protect it with a network security group.
+<#
 Clear-Host      # Workaround for buffer overflow on console, Ref: https://github.com/PowerShell/vscode-powershell/issues/1536
 $rgName = "<name of your new resource group>"
 $locName = (Get-AzResourceGroup -Name $rgName).Location
@@ -34,4 +35,24 @@ $vnet = Get-AzVirtualNetwork -ResourceGroupName $rgName -Name TestLab
 $nsg = Get-AzNetworkSecurityGroup -Name Corpnet -ResourceGroupName $rgName
 Set-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name Corpnet -AddressPrefix "10.0.0.0/24" -NetworkSecurityGroup $nsg
 $vnet | Set-AzVirtualNetwork
+#>
+
+# Create DC1 VM and configure it as the DC for testlab.<your public domain> AD DS domain and a DNS server for the VMs of TestLab virtual network.
+<#
+$rgName="<resource group name>"
+$locName=(Get-AzResourceGroup -Name $rgName).Location
+$vnet=Get-AzVirtualNetwork -Name TestLab -ResourceGroupName $rgName
+$pip=New-AzPublicIpAddress -Name DC1-PIP -ResourceGroupName $rgName -Location $locName -AllocationMethod Dynamic
+$nic=New-AzNetworkInterface -Name DC1-NIC -ResourceGroupName $rgName -Location $locName -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -PrivateIpAddress 10.0.0.4
+$vm=New-AzVMConfig -VMName DC1 -VMSize Standard_B2s
+$cred=Get-Credential -Message "Type the name and password of the local administrator account for DC1."
+$vm=Set-AzVMOperatingSystem -VM $vm -Windows -ComputerName DC1 -Credential $cred -ProvisionVMAgent -EnableAutoUpdate
+$vm=Set-AzVMSourceImage -VM $vm -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2019-Datacenter -Version "latest"
+$vm=Add-AzVMNetworkInterface -VM $vm -Id $nic.Id
+$vm=Set-AzVMOSDisk -VM $vm -Name "DC1-OS" -DiskSizeInGB 128 -CreateOption FromImage
+$diskConfig=New-AzDiskConfig -AccountType "Standard_LRS" -Location $locName -CreateOption Empty -DiskSizeGB 20
+$dataDisk1=New-AzDisk -DiskName "DC1-DataDisk1" -Disk $diskConfig -ResourceGroupName $rgName
+$vm=Add-AzVMDataDisk -VM $vm -Name "DC1-DataDisk1" -CreateOption Attach -ManagedDiskId $dataDisk1.Id -Lun 1
+New-AzVM -ResourceGroupName $rgName -Location $locName -VM $vm
+#>
 
