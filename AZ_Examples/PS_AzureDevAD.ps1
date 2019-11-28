7c2934583d08993f3cf5eaecbac44bfeec3a3fdf
peterennis <peterennis@yahoo.com>
2019-11-27 17:00:06 -0800

    chore(): add list of variables used and commands to make DC1 a domain controller

--- AZ_Examples/PS_AzureDevAD.ps1
+++ AZ_Examples/PS_AzureDevAD.ps1
@@ -2,7 +2,27 @@
 # Ref: http://tritoneco.com/2014/02/21/fix-for-powershell-script-not-digitally-signed/
 # Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
+
+<# VARIABLES USED
+$subscr="<subscription name>"
+$rgName = "<resource group name>"
+$locName = "<location name, such as West US>"
+$corpnetSubnet
+$rule1
+$vnet
+$nsg
+$pip
+$nic
+$vm
+$cred
+$diskConfig
+$dataDisk1
+$yourDomain="<your public domain>"
+#>
+
+
 # Ref: https://docs.microsoft.com/en-us/microsoft-365/enterprise/simulated-ent-base-configuration-microsoft-365-enterprise
+# Method 2: Build your simulated intranet with Azure PowerShell
 
 # Sign in to your Azure account with the following command
 #Connect-AzAccount
@@ -74,3 +94,87 @@ In Password, type the password of the local administrator account, and then clic
 When prompted, click Yes.
 #>
 
+# Next, add an extra data disk as a new volume with the drive letter F: with this command at an administrator-level Windows PowerShell command prompt on DC1.
+#Get-Disk | Where-Object PartitionStyle -eq "RAW" | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "WSAD Data"
+
+<# Sample Command/Output from an administrator-level Windows PowerShell command prompt on DC1:
+```
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+PS C:\Users\peterennis> Get-Disk | Where-Object PartitionStyle -eq "RAW" | Initialize-Disk -PartitionStyle MBR -PassThru | New-
+Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "WSAD Data"
+
+DriveLetter FriendlyName FileSystemType DriveType HealthStatus OperationalStatus SizeRemaining  Size
+----------- ------------ -------------- --------- ------------ ----------------- -------------  ----
+F           WSAD Data    NTFS           Fixed     Healthy      OK                     19.94 GB 20 GB
+
+
+PS C:\Users\peterennis>
+```
+#>
+
+# Next, configure DC1 as a domain controller and DNS server for the testlab.<your public domain> domain.
+# Specify your public domain name, remove the < and > characters, and then run these commands at
+# an administrator-level Windows PowerShell command prompt on DC1.
+
+<#
+$yourDomain = "<your public domain>"
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName testlab.$yourDomain -DatabasePath "F:\NTDS" -SysvolPath "F:\SYSVOL" -LogPath "F:\Logs"
+#>
+
+<#
+You will need to specify a safe mode administrator password. Store this password in a secure location.
+Note that these commands can take a few minutes to complete.
+
+```
+PS C:\Users\peterennis> Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Success Restart Needed Exit Code      Feature Result
+------- -------------- ---------      --------------
+True    No             Success        {Active Directory Domain Services, Group P...
+
+
+SafeModeAdministratorPassword: ************
+Confirm SafeModeAdministratorPassword: ************
+
+The target server will be configured as a domain controller and restarted when this operation is complete.
+Do you want to continue with this operation?
+[Y] Yes  [A] Yes to All  [N] No  [L] No to All  [S] Suspend  [?] Help (default is "Y"):
+WARNING: Windows Server 2019 domain controllers have a default for the security setting named "Allow cryptography
+algorithms compatible with Windows NT 4.0" that prevents weaker cryptography algorithms when establishing security
+channel sessions.
+
+For more information about this setting, see Knowledge Base article 942564
+(http://go.microsoft.com/fwlink/?LinkId=104751).
+
+WARNING: This computer has at least one physical network adapter that does not have static IP address(es) assigned to
+its IP Properties. If both IPv4 and IPv6 are enabled for a network adapter, both IPv4 and IPv6 static IP addresses
+should be assigned to both IPv4 and IPv6 Properties of the physical network adapter. Such static IP address(es)
+assignment should be done to all the physical network adapters for reliable Domain Name System (DNS) operation.
+
+WARNING: A delegation for this DNS server cannot be created because the authoritative parent zone cannot be found or it
+ does not run Windows DNS server. If you are integrating with an existing DNS infrastructure, you should manually
+create a delegation to this DNS server in the parent zone to ensure reliable name resolution from outside the domain
+"testlab.adaept.com". Otherwise, no action is required.
+
+WARNING: Windows Server 2019 domain controllers have a default for the security setting named "Allow cryptography
+algorithms compatible with Windows NT 4.0" that prevents weaker cryptography algorithms when establishing security
+channel sessions.
+
+For more information about this setting, see Knowledge Base article 942564
+(http://go.microsoft.com/fwlink/?LinkId=104751).
+
+etc. some more warning messages about WS 2019 configuration...
+
+#>
+
+<#
+After DC1 restarts, reconnect to the DC1 virtual machine.
+
+In the Azure portal, click Resource Groups > [your resource group name] > DC1 > Connect.
+Run the DC1.rdp file that is downloaded, and then click Connect.
+In Windows Security, click Use another account. In User name, type TESTLAB\[Local administrator account name].
+In Password, type the password of the local administrator account, and then click OK.
+When prompted, click Yes.
+#>
